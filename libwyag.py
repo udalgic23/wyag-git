957b445d5e34e4ecb0d63ad2c08c1a58a3545e35
udalgic23 <udalgic23@ku.edu.tr>
2025-03-21 23:17:51 +0300

    Implement object read and write
    Implemented object reading and writing functions

--- libwyag.py
+++ libwyag.py
@@ -153,7 +153,67 @@ def repo_find(path=".", required=True):
     return repo_find(parent, required)
 
 
+def GitObject:
+    
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
 
+    def serialize(self, repo):
+        raise Exception("Unimplemented!")
+    
+    def deserialize(self, data):
+        raise Exception("Unimplemented")
+
+    def init(self):
+        pass
+
+
+def object_read(repo, sha):
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+    
+    if not os.path.isfile(path):
+        return None
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+        
+        x = raw.find(b' ')
+        fmt = raw[:x]
+        
+        y = raw.find(b"\x00", x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw)-y-1:
+            raise Exception(f"Malformed object {sha}: bad length")
+
+        match fmt:
+            case b'commit'  : c=GitCommit
+            case b'tree'    : c=GitTree
+            case b'tag'     : c=GitTag
+            case b'blob'    : c=GitBlob
+            case _:
+                raise Exception(f"Unknown type {fmt.decode("ascii")} for object {sha}")
+        
+        return c(raw[y+1:])
+
+
+def write_object(obj, repo=None):
+    data = obj.serialize()
+
+    result = obj.fmt + b" " + str(len(data)).encode() + b"\x00" + data
+    
+    sha = haslib.sha1(result).hexdigest()
+
+    if repo:
+        path = repo_file(repo, "objects", sha[:2], sha[2:])
+
+        if not os.path.exists(path):
+            with open(path, "wb") as f:
+                f.write(zlib.compress(result))
+
+    return sha
 
 
 

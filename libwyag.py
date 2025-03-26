d097244d79dba365b2ca4b1d43a3e18be0a0e622
udalgic23 <udalgic23@ku.edu.tr>
2025-03-26 21:46:48 +0300

    Implement show-ref command

--- libwyag.py
+++ libwyag.py
@@ -522,13 +522,59 @@ def tree_checkout(repo, tree, path):
                 f.write(obj.blobdata)
 
 
+def ref_resolve(repo, ref):
+    path = repo_file(repo, ref)
+    
+    if not os.path.isfile(path):
+        return None
+
+    with open(path, "r") as fp:
+        data = fp.read()[:-1]
+        
+    if data.startswith("ref: "):
+        return ref_resolve(repo, data[5:])
+    else:
+        return data
 
 
+def ref_list(repo, path=None):
+    if not path:
+        path = repo_dir(repo, "refs")
+    ret = dict()
+
+    for f in sorted(os.listdir(path)):
+        can = os.path.join(path, f)
+
+        if os.path.isdir(can):
+            ret[f] = ref_list(repo, can)
+        else:
+            ret[f] = ref_resolve(repo, can)
+
+    return ret
 
 
+argsp = argsubparsers.add_parser("show-ref", help="List references.")
+argsp.add_argument("-hash", dest="hash", action="store_true", help="Show hash value")
 
+def cmd_show_ref(args):
+    repo = repo_find()
+    refs = ref_list(repo)
+    if args.hash:  
+        show_ref(repo, refs, prefix="refs")
+    else:
+        show_ref(repo, refs, with_hash=False, prefix="refs")
 
+def show_ref(repo, refs, with_hash=True, prefix=""):
+    if prefix:
+        prefix = prefix + "/"
 
+    for k,v in refs.items():
+        if type(v) == str and with_hash:
+            print(f"{v} {prefix}{k}")
+        elif type(v) == str:
+            print(f"{prefix}{k}")
+        else:
+            show_ref(repo, v, with_hash=with_hash, prefix=f"{prefix}{k}")
 
 
 

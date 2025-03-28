8a12decacb3899e467f45add4b80cb0d7aa10280
udalgic23 <udalgic23@ku.edu.tr>
2025-03-28 23:52:49 +0300

    Implement add command

--- libwyag.py
+++ libwyag.py
@@ -1110,10 +1110,44 @@ def rm(repo, paths, delete=True, skip_missing=True):
     index.entries = kept_entries
     index_write(repo, index)
 
+argsp = argsubparsers.add_parser("add", help = "Add files contents to the index.")
+argsp.add_argument("path", nargs="+", help="Files to add")
 
+def cmd_add(args):
+    repo = repo_find()
+    add(repo, args.path)
 
+def add(repo, paths, delete=True, skip_missing=False):
+    rm(repo, paths, delete=False, skip_missing=True)
+    worktree = repo.worktree + os.sep
 
+    clean_paths = set()
+    for path in paths:
+        abspath = os.path.abspath(path)
+        if not (abspath.startswith(worktree) and os.path.isfile(abspath)):
+            raise Exception(f"Not a file, or outside the worktree: {paths}")
+        relpath = os.path.relpath(abspath, repo.worktree)
+        clean_paths.add((abspath, relpath))
+    
+    index = index_read(repo)
 
+    for (abspath, relpath) in clean_paths:
+        with open(abspath, "rb") as fd:
+            sha = object_hash(fd, b"blob", repo)
+            
+            stat = os.stat(abspath)
+            ctime_s = int(stat.st_ctime)
+            ctime_ns = stat.st_ctime_ns % 10**9
+            mtime_s = int(stat.st_mtime)
+            mtime_ns = stat.st_mtime_ns % 10**9
+
+            entry = GitIndexEntry(ctime=(ctime_s, ctime_ns), mtime=(mtime_s, mtime_ns), dev=stat.st_dev, ino=stat.st_ino,
+                                  mode_type=0b1000, mode_perms=0o644, uid=stat.st_uid, gid=stat.st_gid,
+                                  fsize=stat.st_size, sha=sha, flag_assume_valid=False,
+                                  flag_stage=False, name=relpath)
+            index.entries.append(entry)
+            
+    index_write(repo, index)
 
 
 
